3d257c00872ef43686432d4b8239c289dbf4a3b0
MoeyR <ranm@sheridancollege.ca>
2024-03-26 22:33:01 -0400

    Created post-controller and post-routers

--- controllers/posts-controller.js
+++ controllers/posts-controller.js
@@ -1,6 +1,6 @@
 const knex = require("knex")(require("../knexfile"));
 
-const getOnePost = async (req, res)=>{
+const getOnePost = async (req, res) => {
     try {
         const postFound = await knex
             .select(
@@ -13,7 +13,7 @@ const getOnePost = async (req, res)=>{
                 "posts.post_location"
             )
             .from("posts")
-            .join("users", "posts.user_id", "users.id" )
+            .join("users", "posts.user_id", "users.id")
             .where({"posts.id": req.params.id});
 
         if(postFound.length === 0){
@@ -30,6 +30,49 @@ const getOnePost = async (req, res)=>{
     }
 };
 
+//Get all posts
+const getPosts = async (req, res) => {
+    try {
+        const postsData = await knex
+            .select(
+                "posts.id",
+                "users.user_name",
+                "posts.post_title",
+                "posts.post_content",
+                "posts.post_collects",
+                "posts.post_image",
+                "posts.post_location"
+            )
+            .from("posts")
+            .join("users", "posts.user_id", "users.id")
+        
+        res.status(200).json(postsData);
+    } catch (error) {
+        res.status(500).send(`Unable to fetch posts data: ${error}`)
+    }
+};
+
+
+const addNewPost = async (req, res) => {
+    const {user_name, post_title, post_content, post_collects, post_image, post_location} = req.body;
+
+    if(!post_title || !post_content || !post_collects || !post_image || !post_location){
+        return res.status(400).send("Please provide all required fields");
+    }
+
+    try {
+        const result = await knex("posts").insert(req.body)
+        const newPostId = result[0];
+        const createdPost = await knex("posts").where({id: newPostId});
+        
+        res.status(201).json(createdPost);
+    } catch (error) {
+        res.status(500).send("Error in creating new post");
+    }
+};
+
 module.exports ={
     getOnePost,
+    getPosts,
+    addNewPost,
 };
--- routes/posts-routers.js
+++ routes/posts-routers.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const postsController = require("../controllers/posts-controller");
 
+router.route("/posts").get(postsController.getPosts);
 router.route("/posts/:id").get(postsController.getOnePost);
 
 
